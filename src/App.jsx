a62b51059e842bcc28de91cc62ee16ce68186be2
Amit Kashyap <mrkashyapamit@gamil.com>
2024-12-23 21:45:21 +0530

    form basics

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [fname, setfName] = useState("");
+
+  return (
+    <>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+              setfName("");
+            }}
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            setfName(name);
+          }}
+        >
+          Submit
+        </button>
+        <button
+          onClick={() => {
+            setName("");
+          }}
+        >
+          Reset
+        </button>
+      </form>
+      <p>{fname}</p>
+    </>
+  );
+}
+
+export default App;
