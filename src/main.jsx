11ca3f4dc0e2e28c508636422d730785388cf1af
Amit Kashyap <mrkashyapamit@gamil.com>
2024-12-24 10:23:30 +0530

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import Addfruits from './Addfruits.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(<>
 
     <App />
 
+    <Addfruits />
+    </>
 )
